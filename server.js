de507238faafd8730d8778f6b5abe0a5e0a9d8c3
Mario Junior <drapalskimario@gmail.com>
2021-08-19 23:17:55 -0300

    ajustments to correct endpoint

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/:date', (req, res) => {
+app.get('/api/:date?', (req, res) => {
   let { date } = req.params
   if (!date) {
     let unix = new Date().getTime()
